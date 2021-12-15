d5e250badcc8ebd0b12759465d670f9bb1720e88
Alex <streamgroupru@gmail.com>
2021-12-15 23:50:42 +0600

    First task is done

--- lesson_7_1.py
+++ lesson_7_1.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+# 1. Реализовать класс Matrix (матрица).
+# Обеспечить перегрузку конструктора класса (метод init()),
+# который должен принимать данные (список списков) для формирования матрицы.
+# Подсказка: матрица — система некоторых математических величин, расположенных в
+# виде прямоугольной схемы.
+# Примеры матриц: см. в методичке.
+#
+# Следующий шаг — реализовать перегрузку метода str() для вывода матрицы в привычном виде.
+# Далее реализовать перегрузку метода add() для реализации операции сложения двух объектов
+# класса Matrix (двух матриц). Результатом сложения должна быть новая матрица.
+# Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки
+# первой матрицы складываем с первым элементом первой строки второй матрицы и т.д.
+class Matrix:
+    value: list
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, value: list):
+        self.value = value
+
+    def __add__(self, other):
+        rows_count = len(self.value)
+        items_count = len(self.value[0])
+
+        new_value = [
+            [
+                self.value[row][idx] + other.value[row][idx]
+                for idx in range(items_count)
+            ]
+            for row in range(rows_count)
+        ]
+
+        return Matrix(new_value)
+
+    def __str__(self):
+        return "\n".join(
+            str(row).strip('[]').replace(',', '')
+            for row in self.value
+        )
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+a = Matrix([
+    [15, 14, 13],
+    [12, 11, 10],
+])
 
+b = Matrix([
+    [21, 22, 23],
+    [24, 25, 26],
+])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+c = a + b
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(a)
+print()
+print(b)
+print()
+print(c)
